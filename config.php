e2dc24963fd88f16c8df4f9648842922453a8cd5
Rob <hosting@chaosjester.net>
2017-07-28 14:23:27 +1000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Configuration file for Vesta to Rainloop Sync
+
+// Enter VestaCP Hostname
+$vst_hostname = 'vestacp.domain.com';
+// Enter Admin username, usually admin
+$vst_username = 'admin';
+// Enter Admin accout password
+$vst_password = 'mypassw0rd';
+// Enter Rainloop domain configuration folder
+// Note that the user executing this PHP script needs to have write access to the location
+$rainloopdir = '/path/to/rainloop/data/_data_/_default_/domains/'
+?>
